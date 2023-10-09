20437c2a0206b09fa4f03538e5afe3d876ca6cd7
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-09 18:50:27 +0200

    Add project files.

---
+++ 18.Constructrors/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.Constructrors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book book1 = new Book("Herry Porter", "JK Rowling", 400);
+
+            Console.WriteLine(book1.title);
+            Console.ReadLine();
+        }
+            class Book
+        {
+            public string title;
+            public string author;
+            public int pages;
+
+            public Book(string aTitle, string aAuthor, int aPage)
+            {
+                title = aTitle;
+                author = aAuthor;
+                pages = aPage;
+
+            }
+        }
+    }
+            
+        
+    
+}
